64a18a8625fdbb1627d5a2ae282bde243e203836
Dmitry Bohdanov <starsettime@gmail.com>
2019-07-18 21:30:42 +0300

    Added create-todo feature

--- src/components/create-form/create-form.js
+++ src/components/create-form/create-form.js
@@ -1,11 +1,41 @@
 import React, {Component} from 'react'
 
  export default class CreateForm extends Component {
+
+    state = {
+        text: ''
+    }
+
+    onTextChange = (event) => {
+        this.setState({
+            text: event.target.value
+        })
+    }
+
+
+    onSubmit = (event) => {
+        event.preventDefault();
+        this.props.onItemAdded(this.state.text);
+        this.setState({
+            text: ''
+        })
+    }
+
     render () {
         return (
-            <div>
-                <button onClick = {() => { this.props.onItemAdded("My Text")}} className="btn btn-outline-info float-right mt-4">Add todo</button>
-            </div>
+            <form className="d-flex mt-4"
+            onSubmit={this.onSubmit}
+            >
+                <input type="text"
+                className="form-control mr-3"
+                onChange={this.onTextChange}
+                placeholder="What needs to be done"
+                value={this.state.text}
+                >
+
+                </input>
+                <button className="btn btn-outline-info float-right">Add todo</button>
+            </form>
         )
     }
 }
